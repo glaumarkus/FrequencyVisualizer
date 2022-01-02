25ac890dac5d0f4180c5f58bdb634b4528fd333a
Markus Glau <glaumarkus1209@gmail.com>
2022-01-02 21:28:23 +0100

    ?

--- include/LEDController.h
+++ include/LEDController.h
@@ -6,15 +6,15 @@ constexpr long      TARGET_FREQ     = WS2811_TARGET_FREQ;
 constexpr int       GPIO_PIN        = 18;
 constexpr int       DMA             = 10;
 constexpr uint32_t  STRIP_TYPE      = WS2811_STRIP_GBR;
-constexpr int       LED_COUNT       = 110;
 
 class LEDController
 {
 public:
     
-    LEDController();
+    LEDController(int led_count, int brightness);
     ~LEDController();
 
+    void ChangeBrightness(float brightness);
     void ChangeLED(int idx, uint32_t color);
     void Render();
 
@@ -23,39 +23,3 @@ private:
     ws2811_t m_strip;
 
 };
-
-std::shared_ptr<LEDController> GetController();
-
-
-
-/*
-class LEDController {
-
-public:
-
-    LEDController(const LEDController&) = delete;
-    LEDController(LEDController&&) = delete;
-
-    LEDController& operator =(const LEDController&) = delete;
-    LEDController& operator =(LEDController&&) = delete;
-
-public:
-
-    static LEDController* Get();
-    void ChangeLED(const int& idx, const uint32_t& color);
-    void Render();
-
-private:
-   
-    LEDController();
-    void IChangeLED(const int& idx, const uint32_t& color);
-    void IRender();
-
-private:
-    
-    static LEDController *instance;
-    ws2811_t m_strip;    
-
-};
-
-*/
--- src/LEDController.cpp
+++ src/LEDController.cpp
@@ -1,15 +1,15 @@
 #include <LEDController.h>
 #include <Color.h>
 
-LEDController::LEDController()
+LEDController::LEDController(int led_count, int brightness)
 {
     // setup & init
     m_strip.freq = TARGET_FREQ;
     m_strip.dmanum = DMA;
     m_strip.channel[0].gpionum = GPIO_PIN;
-    m_strip.channel[0].count = LED_COUNT;
+    m_strip.channel[0].count = led_count;
     m_strip.channel[0].invert = 0;
-    m_strip.channel[0].brightness = 255;
+    m_strip.channel[0].brightness = brightness;
     m_strip.channel[0].strip_type = STRIP_TYPE;
     m_strip.channel[1].gpionum = 0;
     m_strip.channel[1].count = 0;
@@ -20,7 +20,7 @@ LEDController::LEDController()
 
 LEDController::~LEDController()
 {
-    for (int i = 0; i < LED_COUNT; i++)
+    for (int i = 0; i < m_strip.channel[0].count; i++)
         ChangeLED(i, uBlack);
     Render();
 }
@@ -35,63 +35,7 @@ void LEDController::Render()
     ws2811_render(&m_strip);
 }
 
-static auto led_controller = std::make_shared<LEDController> ();
-
-std::shared_ptr<LEDController> GetController()
+void LEDController::ChangeBrightness(float brightness)
 {
-    return led_controller;
+    m_strip.channel[0].brightness = brightness;   
 }
-
-
-
-/*
-LEDController* LEDController::Get()
-{
-    if (!instance)
-    instance = new LEDController;
-    return instance;
-}
-
-
-void LEDController::ChangeLED(const int& idx, const uint32_t& color) {
-    return this->IChangeLED(idx, color);
-}
-
-
-void LEDController::Render() {
-    this->IRender();
-}
-
-
-LEDController::LEDController()
-{
-    // setup & init
-    m_strip.freq = TARGET_FREQ;
-    m_strip.dmanum = DMA;
-    m_strip.channel[0].gpionum = GPIO_PIN;
-    m_strip.channel[0].count = LED_COUNT;
-    m_strip.channel[0].invert = 0;
-    m_strip.channel[0].brightness = 255;
-    m_strip.channel[0].strip_type = STRIP_TYPE;
-    m_strip.channel[1].gpionum = 0;
-    m_strip.channel[1].count = 0;
-    m_strip.channel[1].invert = 0;
-    m_strip.channel[1].brightness = 0;
-    ws2811_init(&m_strip);
-}
-
-
-void LEDController::IChangeLED(const int& idx, const uint32_t& color)
-{
-    m_strip.channel[0].leds[idx] = color;
-}
-
-
-void LEDController::IRender()
-{
-    ws2811_render(&m_strip);
-}
-
-
-LEDController* LEDController::instance = 0;
-*/
